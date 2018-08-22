0564cb123e7f6717943ab577299ece1a6ba97c42
Bryan <bjdominguez23@gmail.com>
2018-08-21 21:57:45 -0500

    Mi primer chat

---
+++ routes/chat_servidor.js
@@ -0,0 +1,83 @@
+/*var io;
+
+exports.iniciar = function(http){
+	io = require('socket.io').listen(http);
+	io.sockets.on('connection',function(socket){
+		//Este dispara una llamada cada vez que un nuevo usuario se conecte
+		socket.emit('mensaje',{text:'Bienvenido'});
+		socket.broadcast.emit('mensaje',{text:'Un nuevo usuario se ha conectado'});
+
+		socket.on('disconnect',function(){
+			socket.broadcast.emit('mensaje',{text:'Un usuario se ha desconectado'});
+		});
+	});
+}*/
+//CHAT SERVIDOR
+var nicks = new Array();
+var io;
+exports.iniciar = function(http) {
+ io = require('socket.io').listen(http);
+ io.sockets.on('connection', function(socket) {
+  usuarios(socket);
+  nick(socket);
+  mensaje(socket);
+  usuario_desconectado(socket);
+ });
+}
+
+function usuarios(socket) {
+ socket.emit('usuarios', {
+  nicks: nicks
+ });
+ socket.broadcast.emit('usuarios', {
+  nicks: nicks
+ });
+}
+
+function nick(socket) {
+ socket.on('nick', function(data) {
+  var nick = data.nick;
+  if (nicks.indexOf(nick) == -1) {
+   nicks.push(nick);
+   socket.nick = nick;
+   socket.emit('nick', {
+    correcto: true,
+    nick: nick
+   });
+   socket.broadcast.emit('nuevo_usuario', {
+    nick: nick
+   });
+   usuarios(socket);
+  } else {
+   socket.emit('nick', {
+    correcto: false,
+    nick: nick
+   });
+  }
+ })
+}
+
+function mensaje(socket) {
+ socket.on('mensaje', function(data) {
+  if (socket.nick) {
+   var mensaje = data.mensaje;
+   var nick = socket.nick;
+   socket.broadcast.emit('mensaje', {
+    mensaje: mensaje,
+    nick: nick
+   });
+  }
+ });
+}
+
+function usuario_desconectado(socket) {
+ socket.on('disconnect', function() {
+  if (socket.nick) {
+   nicks.splice(nicks.indexOf(socket.nick), 1);
+   socket.broadcast.emit('disconnect', {
+    nick: socket.nick
+   });
+   usuarios(socket);
+  }
+ });
+}
